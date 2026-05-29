23eade988ee29529b2877873c4e8c34a2ba4ee49
sanfrwl <ghassan.feriwel@gmail.com>
2026-05-30 04:35:25 +0700

    last

--- TUBESALPRO/seagamesManager.go
+++ TUBESALPRO/seagamesManager.go
@@ -90,6 +90,26 @@ func main(){
 				desc = "Ascending"
 				tampilkanKlasemen(N, jumlahNegara, desc)
 			}
+
+			fmt.Println("Ingin mencari negara tertentu?")
+			fmt.Println("1. ya")
+			fmt.Println("2. tidak")
+			fmt.Scan(&kriteriaCari)
+			index =cariNegara(N, nama)
+
+			if idex != -1 {
+				fmt.Printf("| %-3s | %-15s | %-5s | %-5s | %-5s |\n", "No", "Negara", "Emas", "Perak", "Perunggu")
+				Printf("| %-3d | %-15s | %-6d | %-6d | %-6d |\n", index+1, daftar[index].Nama, daftar[index].Emas, daftar[index].Perak, daftar[index].Perunggu)
+			}
+
+
+			if kriteriaCari == 1 {
+				fmt.Println("Masukkan nama negara yang ingin dicari:")
+				fmt.Scan(&nama)
+				cariNegara(N, nama)
+			}else if kriteriaCari == 2 {
+				fmt.Println("Kembali ke menu utama...")
+			}
 		} else if pilihan == 5 {
 			fmt.Println("Terima kasih telah menggunakan SEAGAMES MANAGER!")
 		} else {
@@ -110,6 +130,24 @@ func cariNegaraIndex(N daftarNegara, nama string)int{
 		i++
 	}
 	return index
+}
+
+func cariNegara(N daftarNegara, nama string){
+	var kiri, kanan, tengah int
+	var found int = -1
+	kiri = 0
+	kanan = jumlahNegara - 1
+
+	for kiri <= kanan {
+		tengah = (kiri + kanan) / 2
+		if strings.EqualFold(N[tengah].Nama, nama) {
+			found = tengah
+		} else if strings.ToLower(N[tengah]) < strings.ToLower(nama) {
+			kiri = tengah + 1
+	    } else if strings.ToLower(N[tengah]) > strings.ToLower(nama) {
+			kanan = tengah - 1
+		}
+		return found
 }
 
 // fungsi untuk menambahkan negara baru ke dalam daftar
